e820d856e0afa8da9383800f5821d458d74f124f
CDR4733 <shsing2@naver.com>
2026-01-16 23:07:16 +0900

    [fix] auth의존성 문제 해결
    auth.module에
    [providers] 에 JwtStrategy를 추가

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { USER_MESSAGES } from 'src/constants/user-message.constant';
+import { AuthGuard } from '@nestjs/passport';
+import { LogIn } from 'src/decorators/log-in.decorator';
 
 @ApiTags('02. USER API')
 @Controller('users')
@@ -10,10 +12,11 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   /** 내 정보 조회(R) API **/
+  @UseGuards(AuthGuard('jwt'))
   @ApiBearerAuth()
   @ApiOperation({ summary: '내 정보 조회 API' })
   @Get('me')
-  async myProfile(user: User) {
+  async myProfile(@LogIn() user: User) {
     const data = await this.userService.myProfile(user);
     return {
       status: HttpStatus.OK,
